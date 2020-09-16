e6111ad3f7356ba72093ee3a15bd62879a651bbb
DominiqN <dmut7691@gmail.com>
2020-09-16 22:33:52 +0900

    extract amountFor()

--- statement.ts
+++ statement.ts
@@ -1,4 +1,4 @@
-import { Invoice, Plays } from "./types.ts";
+import { Performance, Invoice, Play, Plays } from "./types.ts";
 
 export function statement(invoice: Invoice, plays: Plays) {
   let totalAmount = 0;
@@ -6,34 +6,18 @@ export function statement(invoice: Invoice, plays: Plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
   const format = (amount: number) => `${amount}원`;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
+  for (let performance of invoice.performances) {
+    const play = plays[performance.playID];
+    let thisAmount = amountFor(performance, play);
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(performance.audience - 30, 0);
     if (play.type === "comedy") {
-      volumeCredits += Math.floor(perf.audience / 5);
+      volumeCredits += Math.floor(performance.audience / 5);
     }
 
-    result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+    result += `${play.name}: ${
+      format(thisAmount / 100)
+    } (${performance.audience}석)\n`;
     totalAmount += thisAmount;
   }
 
@@ -41,4 +25,28 @@ export function statement(invoice: Invoice, plays: Plays) {
   result += `적립 포인트: ${volumeCredits}점`;
 
   return result;
+
+  function amountFor(performance: Performance, play: Play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (performance.audience > 30) {
+          thisAmount += 1000 * (performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (performance.audience > 20) {
+          thisAmount += 10000 + 500 * (performance.audience - 20);
+        }
+        thisAmount += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return thisAmount;
+  }
 }
